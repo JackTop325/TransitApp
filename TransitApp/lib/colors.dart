f2c61bffc429032b6b6478d2efe1fcc29b54c706
Kyle Alvares <kyle@Kyles-MacBook-Air.local>
2022-11-09 07:23:55 -0500

    colors

--- TransitApp/lib/colors.dart
+++ TransitApp/lib/colors.dart
@@ -2,3 +2,114 @@ import 'package:flutter/material.dart';
 
 const drtGreen = Color.fromARGB(255, 0, 112, 60);
 const drtGray = Color.fromARGB(255, 95, 96, 98);
+const drtOrange = Color.fromARGB(255, 220, 134, 51);
+
+const ibmRed100 = Color.fromARGB(255, 45, 7, 9);
+const ibmRed90 = Color.fromARGB(255, 82, 4, 8);
+const ibmRed80 = Color.fromARGB(255, 117, 14, 19);
+const ibmRed70 = Color.fromARGB(255, 162, 25, 31);
+const ibmRed60 = Color.fromARGB(255, 218, 30, 40);
+const ibmRed50 = Color.fromARGB(255, 250, 77, 86);
+const ibmRed40 = Color.fromARGB(255, 255, 131, 137);
+const ibmRed30 = Color.fromARGB(255, 255, 179, 184);
+const ibmRed20 = Color.fromARGB(255, 255, 215, 217);
+const ibmRed10 = Color.fromARGB(255, 255, 241, 241);
+
+const ibmMagenta100 = Color.fromARGB(255, 42, 10, 24);
+const ibmMagenta90 = Color.fromARGB(255, 81, 2, 36);
+const ibmMagenta80 = Color.fromARGB(255, 116, 9, 55);
+const ibmMagenta70 = Color.fromARGB(255, 159, 24, 83);
+const ibmMagenta60 = Color.fromARGB(255, 208, 38, 112);
+const ibmMagenta50 = Color.fromARGB(255, 238, 83, 150);
+const ibmMagenta40 = Color.fromARGB(255, 255, 126, 182);
+const ibmMagenta30 = Color.fromARGB(255, 255, 175, 210);
+const ibmMagenta20 = Color.fromARGB(255, 255, 214, 232);
+const ibmMagenta10 = Color.fromARGB(255, 255, 240, 247);
+
+const ibmPurple100 = Color.fromARGB(255, 28, 15, 48);
+const ibmPurple90 = Color.fromARGB(255, 49, 19, 94);
+const ibmPurple80 = Color.fromARGB(255, 73, 29, 139);
+const ibmPurple70 = Color.fromARGB(255, 105, 41, 196);
+const ibmPurple60 = Color.fromARGB(255, 138, 63, 252);
+const ibmPurple50 = Color.fromARGB(255, 165, 110, 255);
+const ibmPurple40 = Color.fromARGB(255, 190, 149, 255);
+const ibmPurple30 = Color.fromARGB(255, 212, 187, 255);
+const ibmPurple20 = Color.fromARGB(255, 232, 218, 255);
+const ibmPurple10 = Color.fromARGB(255, 246, 242, 255);
+
+const ibmBlue100 = Color.fromARGB(255, 0, 17, 65);
+const ibmBlue90 = Color.fromARGB(255, 0, 29, 108);
+const ibmBlue80 = Color.fromARGB(255, 0, 45, 156);
+const ibmBlue70 = Color.fromARGB(255, 0, 67, 206);
+const ibmBlue60 = Color.fromARGB(255, 15, 98, 254);
+const ibmBlue50 = Color.fromARGB(255, 69, 137, 255);
+const ibmBlue40 = Color.fromARGB(255, 120, 169, 255);
+const ibmBlue30 = Color.fromARGB(255, 166, 200, 255);
+const ibmBlue20 = Color.fromARGB(255, 208, 226, 255);
+const ibmBlue10 = Color.fromARGB(255, 237, 245, 255);
+
+const ibmCyan100 = Color.fromARGB(255, 6, 23, 39);
+const ibmCyan90 = Color.fromARGB(255, 1, 39, 73);
+const ibmCyan80 = Color.fromARGB(255, 0, 58, 109);
+const ibmCyan70 = Color.fromARGB(255, 0, 83, 154);
+const ibmCyan60 = Color.fromARGB(255, 0, 114, 195);
+const ibmCyan50 = Color.fromARGB(255, 17, 146, 232);
+const ibmCyan40 = Color.fromARGB(255, 51, 177, 255);
+const ibmCyan30 = Color.fromARGB(255, 130, 207, 255);
+const ibmCyan20 = Color.fromARGB(255, 186, 230, 255);
+const ibmCyan10 = Color.fromARGB(255, 229, 246, 255);
+
+const ibmTeal100 = Color.fromARGB(255, 8, 26, 28);
+const ibmTeal90 = Color.fromARGB(255, 2, 43, 48);
+const ibmTeal80 = Color.fromARGB(255, 0, 65, 68);
+const ibmTeal70 = Color.fromARGB(255, 0, 93, 93);
+const ibmTeal60 = Color.fromARGB(255, 0, 125, 121);
+const ibmTeal50 = Color.fromARGB(255, 0, 157, 154);
+const ibmTeal40 = Color.fromARGB(255, 8, 189, 186);
+const ibmTeal30 = Color.fromARGB(255, 61, 219, 217);
+const ibmTeal20 = Color.fromARGB(255, 158, 240, 240);
+const ibmTeal10 = Color.fromARGB(255, 217, 251, 251);
+
+const ibmGreen100 = Color.fromARGB(255, 7, 25, 8);
+const ibmGreen90 = Color.fromARGB(255, 2, 45, 13);
+const ibmGreen80 = Color.fromARGB(255, 4, 67, 23);
+const ibmGreen70 = Color.fromARGB(255, 14, 96, 39);
+const ibmGreen60 = Color.fromARGB(255, 25, 128, 56);
+const ibmGreen50 = Color.fromARGB(255, 36, 161, 72);
+const ibmGreen40 = Color.fromARGB(255, 66, 190, 101);
+const ibmGreen30 = Color.fromARGB(255, 111, 220, 140);
+const ibmGreen20 = Color.fromARGB(255, 167, 240, 186);
+const ibmGreen10 = Color.fromARGB(255, 222, 251, 230);
+
+const ibmCoolGray100 = Color.fromARGB(255, 18, 22, 25);
+const ibmCoolGray90 = Color.fromARGB(255, 33, 39, 42);
+const ibmCoolGray80 = Color.fromARGB(255, 52, 58, 63);
+const ibmCoolGray70 = Color.fromARGB(255, 77, 83, 88);
+const ibmCoolGray60 = Color.fromARGB(255, 105, 112, 119);
+const ibmCoolGray50 = Color.fromARGB(255, 135, 141, 150);
+const ibmCoolGray40 = Color.fromARGB(255, 162, 169, 176);
+const ibmCoolGray30 = Color.fromARGB(255, 193, 199, 205);
+const ibmCoolGray20 = Color.fromARGB(255, 221, 225, 230);
+const ibmCoolGray10 = Color.fromARGB(255, 242, 244, 248);
+
+const ibmGray100 = Color.fromARGB(255, 22, 22, 22);
+const ibmGray90 = Color.fromARGB(255, 38, 38, 38);
+const ibmGray80 = Color.fromARGB(255, 57, 57, 57);
+const ibmGray70 = Color.fromARGB(255, 82, 82, 82);
+const ibmGray60 = Color.fromARGB(255, 111, 111, 111);
+const ibmGray50 = Color.fromARGB(255, 141, 141, 141);
+const ibmGray40 = Color.fromARGB(255, 168, 168, 168);
+const ibmGray30 = Color.fromARGB(255, 198, 198, 198);
+const ibmGray20 = Color.fromARGB(255, 224, 224, 224);
+const ibmGray10 = Color.fromARGB(255, 244, 244, 244);
+
+const ibmWarmGray100 = Color.fromARGB(255, 23, 20, 20);
+const ibmWarmGray90 = Color.fromARGB(255, 39, 37, 37);
+const ibmWarmGray80 = Color.fromARGB(255, 60, 56, 56);
+const ibmWarmGray70 = Color.fromARGB(255, 86, 81, 81);
+const ibmWarmGray60 = Color.fromARGB(255, 114, 110, 110);
+const ibmWarmGray50 = Color.fromARGB(255, 143, 139, 139);
+const ibmWarmGray40 = Color.fromARGB(255, 173, 168, 168);
+const ibmWarmGray30 = Color.fromARGB(255, 202, 197, 196);
+const ibmWarmGray20 = Color.fromARGB(255, 229, 224, 223);
+const ibmWarmGray10 = Color.fromARGB(255, 247, 243, 242);
