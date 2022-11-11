38acae5ca047e78524635d49bf9e5724fd24fc01
Kyle-Alvares <55460977+Kyle-Alvares@users.noreply.github.com>
2022-11-11 17:25:19 -0500

    local storage schedule

---
+++ TransitApp/lib/local_storage/schedule_model.dart
@@ -0,0 +1,46 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'db_utils.dart';
+import 'schedule.dart';
+
+class ScheduleModel {
+  Future insertSchedule(Schedule schedule) async {
+    final db = await DBUtils.init();
+    return db.insert(
+      'schedule_items',
+      schedule.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future getAllSchedules() async {
+    final db = await DBUtils.init();
+    final List<Map<String, dynamic>> maps = await db.query('schedule_items');
+    List<Schedule> result = [];
+    if (maps.isNotEmpty) {
+      for (int i = 0; i < maps.length; i++) {
+        result.add(Schedule.fromMap(maps[i]));
+      }
+    }
+    return result;
+  }
+
+  Future updateSchedule(Schedule schedule) async {
+    final db = await DBUtils.init();
+    await db.update(
+      'grade_items',
+      schedule.toMap(),
+      where: 'id = ?',
+      whereArgs: [schedule.id],
+    );
+  }
+
+  Future deleteScheduleWithId(int id) async {
+    final db = await DBUtils.init();
+    await db.delete(
+      'schedule_items',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
